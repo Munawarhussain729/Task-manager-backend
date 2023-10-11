592aadcd4dc9b5cbd7f082c8134885ec62852788
Munawar Hussain <munawarhussa.7@gmail.com>
2023-10-11 19:49:52 +0500

    Add project model APIs

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const ProjectModel = require('../models/ProjectModel')
+const Users = require('../models/UserModel')
+const Task = require('../models/TaskModel')
 
 const createProject = async (req, res) => {
     try {
@@ -28,14 +30,97 @@ const getProjectDetails = async (req, res) => {
     try {
         const projectTitle = req?.body?.title
         console.log("Project title is ", projectTitle);
-        const projects = await ProjectModel.find({title:projectTitle})
+        const projects = await ProjectModel.find({ title: projectTitle })
         res.status(200).json({ projects: projects })
     } catch (error) {
         res.status(400).json({ message: error.message })
     }
 }
+
+const getProjectTasks = async (req, res) => {
+    try {
+        const projectId = req?.body?.projectId
+        const project = await ProjectModel.findById(projectId)
+        if (!project) {
+            res.status(400).json({ message: 'Project not found' })
+        }
+        else {
+            const tasks = await Task.find({ _id: { $in: project.tasks } })
+            res.status(200).json({ tasks: tasks })
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const addTask = async (req, res) => {
+    try {
+        const taskId = req?.body?.taskId
+        const projectId = req?.body?.projectId
+        const project = await ProjectModel.findById(projectId)
+        if (project) {
+            if (project.tasks.includes(taskId)) {
+                res.status(200).json({ message: 'task already in project' })
+            }
+            else {
+                const project = await ProjectModel.findByIdAndUpdate(projectId,
+                    { $push: { tasks: taskId } },
+                    { new: true })
+
+                res.status(200).json({ project: project })
+            }
+        }
+        else {
+            res.status(400).json({ message: 'No project found' })
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const addUser = async (req, res) => {
+    try {
+        const userId = req?.body?.userId
+        const projectId = req?.body?.projectId
+
+        const project = await ProjectModel.findById(projectId)
+        if (!project) {
+            return res.status(400).json({ message: 'No Project found' })
+        }
+        if (project?.users?.includes(userId)) {
+            return res.status(400).json({ message: 'User already in project' })
+        }
+        const newProject = await ProjectModel.findByIdAndUpdate(projectId,
+            { $push: { users: userId } },
+            { new: true }
+        )
+        res.status(200).json({ project: newProject })
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const getProjectUsers = async (req, res) => {
+    try {
+        const projectId = req?.body?.projectId
+        const project = await ProjectModel.findById(projectId)
+        if (!project) {
+            res.status(400).json({ message: 'Project not found' })
+        } else {
+            const users = await Users.find({ _id: { $in: project.users } }).exec()
+            res.status(200).json({ users: users })
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
 module.exports = {
     createProject,
     getAllProject,
     getProjectDetails,
+    addTask,
+    getProjectTasks,
+    getProjectUsers,
+    addUser
 }
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -3,8 +3,12 @@ const ProjectController = require('../controllers/ProjectController')
 
 const router = express.Router()
 
+router.post('/add-task', ProjectController.addTask)
+router.post('/add-user', ProjectController.addUser)
 router.post('/create', ProjectController.createProject)
 router.get('/projects', ProjectController.getAllProject)
 router.get('/get-project', ProjectController.getProjectDetails)
+router.get('/project-tasks', ProjectController.getProjectTasks)
+router.get('/project-users', ProjectController.getProjectUsers)
 
 module.exports = router
